13a915aeaaa3d2eab50545553f141bb645b8afa4
Ian Kynnersley <ian@IanMacBookPro.local>
2014-11-24 18:28:30 +0000

    Initial set of dotFiles

---
+++ link_me_up.sh
@@ -0,0 +1,9 @@
+
+ln -s ~/dotFiles/.aliases ~/.aliases
+ln -s ~/dotFiles/.bash_completion ~/.bash_completion
+ln -s ~/dotFiles/.bash_profile ~/.bash_profile
+ln -s ~/dotFiles/.cliprompt ~/.cliprompt
+ln -s ~/dotFiles/.gemrc ~/.gemrc
+ln -s ~/dotFiles/.netrc ~/.netrc
+ln -s ~/dotFiles/.rspec ~/.rspec
+ln -s ~/dotFiles/.vimrc.local ~/.vimrc.local
